ca245c6ab3433cef32032e3fa9a8d91f98c86faf
kirill <kenueyy@yandex.ru>
2025-04-09 22:48:47 +0700

    Update AdPlatformAPI.cs

--- AdPlatformAPI.cs
+++ AdPlatformAPI.cs
@@ -1,5 +1,6 @@
 using AdPlatformsTestWork.Controllers;
 using AdPlatformsTestWork.Utilities;
+using StackExchange.Redis;
 
 public class AdPlatformAPI
 {
@@ -7,11 +8,22 @@ public class AdPlatformAPI
     {
         var builder = WebApplication.CreateBuilder();
         builder.Services.AddControllers();
-        builder.Services.AddSingleton<PlatformRepository>();
         
+#if CACHE_ENABLED
+        
+        builder.Services.AddSingleton<IConnectionMultiplexer>(
+            ConnectionMultiplexer.Connect($"{Globals.CACHE_SERVER_IP}:{Globals.REDIS_PORT}")
+        );
+        builder.Services.AddScoped<RedisCleaner>();
         builder.Services.AddStackExchangeRedisCache(options => {
             options.Configuration = $"{Globals.CACHE_SERVER_IP}:{Globals.REDIS_PORT}";
+            options.InstanceName = "location";
         });
+        
+        builder.Services.AddScoped<PlatformRepository>();
+#else        
+        builder.Services.AddSingleton<PlatformRepository>();
+#endif
         
         var app = builder.Build();
         app.MapControllers();
