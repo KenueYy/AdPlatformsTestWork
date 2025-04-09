10817fec703e8a7023c5d777ce988acf8360908f
kirill <kenueyy@yandex.ru>
2025-04-09 22:48:38 +0700

    Add Cache

--- Repositories/PlatformRepository.cs
+++ Repositories/PlatformRepository.cs
@@ -1,46 +1,96 @@
 ﻿using System.Text;
+using System.Text.Json;
 using AdPlatformsTestWork.Controllers.Utilities;
 using AdPlatformsTestWork.Models;
+using AdPlatformsTestWork.Utilities;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace AdPlatformsTestWork.Controllers;
 
 public class PlatformRepository
 {
-    private readonly IDistributedCache _cache;
     private readonly IDataNormalizer _dataNormalizer;
+    private readonly RedisCleaner _cleaner;
     
     private IDictionary<string, List<string>> _normalizePlatforms;
     
-    public PlatformRepository()
+#if CACHE_ENABLED
+    private readonly IDistributedCache _cache;
+    public PlatformRepository(IDistributedCache cache, RedisCleaner cleaner)
     {
-        //_cache = cache;
+        _cache = cache;
+        _cleaner = cleaner;
         _dataNormalizer = new JsonDataNormalizer();
     }
+    
+#else
+    
+    public PlatformRepository()
+    {
+        _dataNormalizer = new JsonDataNormalizer();
+    }
+    
+#endif
 
-    public void Load(Platforms platforms)
+    public async Task Load(Platforms platforms)
     {
         _normalizePlatforms = _dataNormalizer.Normalize(platforms);
+#if CACHE_ENABLED
+        await _cleaner.FlushAllAsync();
+        foreach (var platform in _normalizePlatforms)
+        {
+            await SetInCache(platform.Key, JsonSerializer.Serialize(platform.Value));
+        }
+#endif
     }
 
-    public JsonContent Search(string location)
+    public async Task<List<string>> Search(string location)
     {
         var locationSegments = location.Trim('/').Split('/');
         var platforms = new List<string>();
-        
+        var prefixLocation = new StringBuilder();
+
         foreach (var segment in locationSegments)
         {
-            var prefixLocation = new StringBuilder("/");
-            prefixLocation.Append(segment);
-            if (_normalizePlatforms.TryGetValue(prefixLocation.ToString(), out var locationPlatforms))
+            prefixLocation.Append('/').Append(segment);
+            var key = prefixLocation.ToString();
+
+#if CACHE_ENABLED
+            var cachedPlatforms = await GetInCache(key);
+            if (cachedPlatforms.Count > 0)
+            {
+                platforms.AddRange(cachedPlatforms);
+            }
+#else
+            if (_normalizePlatforms.TryGetValue(key, out var locationPlatforms))
             {
                 platforms.AddRange(locationPlatforms);
-                
-                return JsonContent.Create(platforms);
             }
-            return JsonContent.Create("location is not found in cache");
+#endif
         }
-        
-        return JsonContent.Create("location is empty");
+
+        return platforms;
     }
+    
+#if CACHE_ENABLED
+
+    private async Task SetInCache(string key, string value)
+    {
+        await _cache.SetStringAsync(key, value, new DistributedCacheEntryOptions());
+    }
+
+    private async Task<List<string>> GetInCache(string key)
+    {
+        var str = await _cache.GetStringAsync(key);
+        if (string.IsNullOrEmpty(str))
+        {
+            return new List<string>();
+        }
+
+        var platforms = JsonSerializer.Deserialize<List<string>>(str);
+        return platforms ?? new List<string>();
+    }
+    
+#endif 
+    
 }
